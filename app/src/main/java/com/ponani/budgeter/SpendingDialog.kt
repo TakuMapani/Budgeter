117bab97bd5238099fabc59edbece3089fbc8dea
TakuMapani <takmapani@gmail.com>
2020-01-12 09:41:11 +1100

    Created layout for dialog box used in entering spending items

---
+++ app/src/main/java/com/ponani/budgeter/SpendingDialog.kt
@@ -0,0 +1,13 @@
+package com.ponani.budgeter
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class SpendingDialog : DialogFragment() {
+
+    //todo: create custom dialog using dialog_spending layout
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
